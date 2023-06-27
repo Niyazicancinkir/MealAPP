29ef63886066a328f43460d44c99f8ce46c4f53e
Your Name <you@example.com>
2023-06-27 13:02:06 +0300

    redux done

--- App.js
+++ App.js
@@ -8,7 +8,9 @@ import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsDetailsScreen from "./screens/MealsDetailsScreen";
 import FavoriteScreen from "./screens/FavoritesScreen";
-import FavoritesContextProvider from "./store/context/Favorites-context";
+//import FavoritesContextProvider from "./store/context/Favorites-context";
+import { Provider } from "react-redux";
+import { store } from "./store/redux/store";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -53,7 +55,8 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store}>
         <NavigationContainer>
           <Stack.Navigator
             screenOptions={{
@@ -83,7 +86,8 @@ export default function App() {
             />
           </Stack.Navigator>
         </NavigationContainer>
-      </FavoritesContextProvider>
+      </Provider>
+      {/* </FavoritesContextProvider> */}
     </>
   );
 }
